b4643f2f690330d1d8bbc8d1600d0a4089c502ee
ujjwal chettri <uchhetri2@myseneca.ca>
2024-10-11 22:35:08 -0400

    update again

--- server.js
+++ server.js
@@ -6,141 +6,98 @@
 * 
 *  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
 * 
-*  Name: Bibek Poudel
-*  Student ID: 157056227
+*  Name: Ujjwal Chhetri
+*  Student ID: 158798223
 *  Date: 2024/10/11
 ********************************************************************************/
 
 const express = require("express");
 const legoData = require("./modules/legoSets");
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 8080;
+const Sequelize = require('sequelize'); // Assuming this is needed for legoData initialization
 
-require('pg'); 
-const Sequelize = require('sequelize');
+// Serve static files
+app.use(express.static(path.join(__dirname, '/public')));
 
+// Set views directory for rendering HTML files
+app.set('views', path.join(__dirname, '/views'));
 
-app.use(express.static(__dirname + '/public'));
-
-app.set('views', __dirname + '/views');
-
-
+// Initialize the Lego data and start the server
 legoData.initialize().then(() => {
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+        console.log(`Server is up and running on port ${PORT}`);
     });
 }).catch((error) => {
     console.error("Failed to initialize lego data: ", error);
 });
 
-
+// Home route - serving home page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
-  });
-  
-  app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/about.html'));
-  });
-  
+});
 
-  app.get('/lego/sets', async(req, res) => {
-    const theme = req.query.theme;
-const data = await legoData.getAllSets();
-console.log(data);
-    
+// About route - serving about page
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/about.html'));
+});
+
+// Fetch all Lego sets or filter by theme
+app.get('/lego/sets', async (req, res) => {
     try {
+        const theme = req.query.theme;
+        const sets = await legoData.getAllSets();
+
         if (theme) {
-            const filteredSets = data.filter(set => set.theme.toLowerCase() === theme.toLowerCase());
+            const filteredSets = sets.filter(set => set.theme.toLowerCase() === theme.toLowerCase());
             if (filteredSets.length > 0) {
                 res.json(filteredSets);
             } else {
                 res.status(404).send(`No Lego sets found for theme: ${theme}`);
             }
         } else {
-            res.json(data);
+            res.json(sets);
         }
     } catch (error) {
-        res.status(404).send('Error fetching Lego sets');
+        console.error("Error fetching Lego sets: ", error);
+        res.status(500).send('Internal server error');
     }
-   
 });
 
-
-
-app.get('/lego/sets/:set_num', async(req, res) => {
-    const setNum = req.params.set_num;
-    const data = await legoData.getAllSets(); 
-
+// Fetch a specific Lego set by set number
+app.get('/lego/sets/:set_num', async (req, res) => {
     try {
-        const set = data.find(s => s.set_num === setNum);
+        const setNum = req.params.set_num;
+        const sets = await legoData.getAllSets();
+        const set = sets.find(s => s.set_num === setNum);
+
         if (set) {
             res.json(set);
         } else {
             res.status(404).send(`Lego set with number ${setNum} not found.`);
         }
     } catch (error) {
-        res.status(404).send('Error fetching Lego set');
+        console.error("Error fetching Lego set: ", error);
+        res.status(500).send('Internal server error');
     }
 });
 
-  
-
-  app.get('/lego/sets/:id', async(req, res) => {
-    const data = await legoData.getSetByNum();
-    const set = data.find(set => set.set_num === req.params.id);
-    if (set) {
-      res.json(set);
-    } else {
-      res.status(404).send('Lego set not found');
-    }
-  });
-  
-  app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
-  });
-  
-
-// app.get("/", (req, res) => {
-//     res.send("Assignment 2: Bibek Poudel - 157056227");
-// });
-
-// app.get("/lego/sets", (req, res) => {
-//     legoData.getAllSets().then((sets) => {
-//         res.json(sets);
-//     }).catch((error) => {
-//         res.status(500).send(error);
-//     });
-// });
-
-
-// app.get("/lego/sets/num-demo", (req, res) => {
-//     legoData.getSetByNum("71002-5").then((set) => { 
-//         res.json(set);
-//     }).catch((error) => {
-//         res.status(500).send(error);
-//     });
-// });
-
-
-// app.get("/lego/sets/theme-demo", (req, res) => {
-//     legoData.getSetsByTheme("Holiday").then((sets) => { 
-//         res.json(sets);
-//     }).catch((error) => {
-//         res.status(500).send(error);
-//     });
-// });
-
-
+// Directory listing route
 app.get("/files", (req, res) => {
-    const directoryPath = path.join(__dirname, "data"); 
+    const directoryPath = path.join(__dirname, "data");
 
     fs.readdir(directoryPath, (err, files) => {
         if (err) {
-            return res.status(500).send("Unable to scan directory: " + err);
+            console.error("Error reading directory: ", err);
+            return res.status(500).send("Unable to scan directory");
         }
-
-        
         res.json(files);
     });
+});
+
+// Catch-all route for 404 errors
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
 });
